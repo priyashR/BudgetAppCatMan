10e9c51f8887681a7b0b66bc853df9b6f81c1229
priyashR <priyash.ramawthar@gmail.com>
2020-02-10 23:22:11 +0200

    display uncat

--- src/main/java/com/gmail/ramawthar/priyash/categories/controller/UIController.java
+++ src/main/java/com/gmail/ramawthar/priyash/categories/controller/UIController.java
@@ -1,21 +1,30 @@
 package com.gmail.ramawthar.priyash.categories.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.gmail.ramawthar.priyash.categories.model.Categories;
 import com.gmail.ramawthar.priyash.categories.services.CategoriesService;
 
-@RestController
+@Controller
 public class UIController {
 	@Autowired
 	CategoriesService categoriesService;
@@ -30,4 +39,11 @@ public class UIController {
 			//logger.info("Controller call to fetchPath");
 	    return new ResponseEntity<>(HttpStatus.OK);
 	  }
+ 	  
+ 	 @GetMapping("/")
+ 	public String index(Model model) {
+ 		model.addAttribute("unCatCategories", categoriesService.getAllUncategorised());
+ 	    return "maintenance";
+ 	}
+
 }
